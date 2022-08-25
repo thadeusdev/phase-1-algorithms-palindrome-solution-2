4b35bda17c402c4f8ae85a2357f36ae93308cc3c
thadeus <thadeusnyariki94@gmail.com>
2022-08-25 23:37:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i < word.length / 2; i++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  // Add your pseudocode here
+  // iterate from the beginning of the string to the middle of the string
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  // if the letters don't match, return false
+
+  // if we reach the middle, and all the letters match, return true
 */
 
 /*
